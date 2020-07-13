d678617d67d1e0c5bc80e821c8cd24c9bcff8c15
Kevin Otuteye <kevinotuteye@gmail.com>
2020-07-13 22:21:11 +0000

    added mock data to vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,30 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    recentLinks: [
+      {
+        short: 'xzcvgfds',
+        original: 'https://www.google.com'
+      },
+      {
+        short: 'r4e2sd6y',
+        original: 'https://www.facebook.com'
+      },
+      {
+        short: 'vlghoesa',
+        original: 'https://www.techcrunch.com'
+      },
+      {
+        short: 'rottieiw',
+        original: 'https://web.whatsapp.com'
+      }
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
-});
+})
