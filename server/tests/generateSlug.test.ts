239bbdb5a27a8bcf17b417e96baa71ea5c3cc7d8
Kevin Otuteye <kevinotuteye@gmail.com>
2020-07-15 18:00:31 +0000

    Wrote new test for generateSLug

--- server/tests/generateSlug.test.ts
+++ server/tests/generateSlug.test.ts
@@ -1,10 +1,12 @@
 import { generateSlug } from '../src/hash/generateSlug'
 import { SlugTuple } from '../src/hash/SlugTuple'
 
-// test('should return slug', () => {
-//   let obj = new SlugTuple('ZDNkNjJk', 'https://www.google.com')
-//   expect(generateSlug('https://www.google.com').next().value).toBe(obj)
-// })
+test('should return slug', () => {
+	let obj = new SlugTuple('ZDNkNjJk', 'https://www.google.com')
+	expect(
+		JSON.stringify(generateSlug('https://www.google.com').next().value)
+	).toBe(JSON.stringify(obj))
+})
 
 // function checkAlternativeSlug(index: number): string {
 //   const hook: Generator = generateSlug('https://www.google.com')
