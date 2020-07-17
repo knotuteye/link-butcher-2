2f19eb27a7c7b4f1b4d28d8d9ccf13283e4e2e2f
Kevin Otuteye <kevinotuteye@gmail.com>
2020-07-17 12:52:24 +0000

    cleaned out production

--- dist/src/try.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var generateSlug = require("./generateSlug");
-var hook = generateSlug.default('https://www.google.com');
-var curr = hook.next();
-while (!curr.done) {
-    console.log(curr.value);
-    curr = hook.next();
-}
