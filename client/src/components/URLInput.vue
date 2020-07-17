4630f6f8d362637fe698a31a8607b18f6c19d476
Kevin Otuteye <kevinotuteye@gmail.com>
2020-07-17 08:20:14 +0000

    more advanced url validation with regex

--- client/src/components/URLInput.vue
+++ client/src/components/URLInput.vue
@@ -24,6 +24,18 @@ export default class URLInput extends Vue {
       this.$store.commit('updateNewLink', tuple)
     }
   }
+  validURL(str: string) {
+    var pattern = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    ) // fragment locator
+    return !!pattern.test(str)
+  }
 }
 </script>
 
