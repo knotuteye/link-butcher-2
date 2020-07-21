5eccfcb8929c6c2573a3f75effe5659d1c46902b
Kevin Otuteye <kevinotuteye@gmail.com>
2020-07-21 12:39:16 +0000

    commented index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+/** Module Imports */
 import express = require('express')
 import cors = require('cors')
 import bodyParser = require('body-parser')
 
+/** Local Imports */
 import {
   getAllTuples,
   getURLOfExistingSlugTuple,
@@ -13,16 +15,14 @@ import { SlugTuple } from './src/hash/SlugTuple'
 
 const app: express.Application = express()
 
+/** Setting Up Middleware */
 app.use(cors())
 app.use(express.static('./client/dist'))
 app.use(bodyParser.json())
 
-/**SLUG ENDPOINTS */
-
+/** API Endpoints */
 app.post('/slugs/create', function (req, res) {
   let url = req.body.url?.toString() // Convert url to string
-
-  // console.log(url)
 
   let slugTuple: SlugTuple | null
 
@@ -37,10 +37,7 @@ app.post('/slugs/create', function (req, res) {
       URLAlreadyExists(slugTuple)
         .then(async (bool) => {
           while (bool) {
-            // console.log('Recursive Generation Entered')
             slugTuple = hook.next().value
-            // console.log(slugTuple?.slug)
-
             bool = await URLAlreadyExists(slugTuple)
           }
         })
@@ -60,11 +57,12 @@ app.post('/slugs/create', function (req, res) {
   })
 })
 
+/** Fetch Recent Links */
 app.post('/slugs/all', async function (req, res) {
   res.json(await getAllTuples())
 })
 
-/**REDIRECT ENDPOINT */
+/** Redirection */
 app.get('/:slug', async (req, res) => {
   let url = await getURLOfExistingSlugTuple(req.params.slug)
   url
@@ -72,10 +70,7 @@ app.get('/:slug', async (req, res) => {
     : res.send("<h1> This link doesn't exist ...yet </h1>")
 })
 
-app.get('/', (req, res) => {
-  res.send('<h1>URL Shrink Backend</h1>')
-})
-
+/** Listener */
 app.listen(process.env.PORT || 5000, function () {
   console.log(`URL Shortener live !`)
 })
